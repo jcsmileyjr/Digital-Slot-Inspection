7f70b59f8764cd73bf4168a6d5b44c24632e73ba
JC <jcsmileyjr@gmail.com>
2017-07-23 14:16:15 -0500

    Inital application setup files
    1. The inital index.html and app.js used to start the development of the
    navigation feature. A libs folder containing relevent support files was
    copied from previous projects.

---
+++ app.js
@@ -0,0 +1,2 @@
+//Sets the variable myApp as a Angular applicatation named 'DSI'. The module ui-router is injected as a dependency to provide url routing. 
+var myApp = angular.module('DSI', ['ui.router']);
