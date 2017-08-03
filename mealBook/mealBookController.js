8b853c06e9e5c2cc21894d576386f9bb4803ad6f
JC <jcsmileyjr@gmail.com>
2017-08-02 21:09:37 -0500

    MealBook Feature: .html and controller
    The mealBook.html file displays a list of MealBook compliance issues
    with checkboxs.
    
    The mealBookController.js file provides the functionality for the
    checkboxs by add/subtract to the issues counter and add/subtract from
    the array of issues found.

---
+++ mealBook/mealBookController.js
@@ -0,0 +1,33 @@
+myApp.controller('mealBookController',  function($scope, $state, MealBook, IssueCounter){
+    
+    //Loads the Mealbook compliance issues used in the mealbook.html checkboxes
+    $scope.mealBook = MealBook.getMealBook();
+    
+    //function used in the checked() function to add mealbook compliance issues to the mealBookTracker and update the issue counter.
+    $scope.addMealBookIssue = function(number){
+        IssueCounter.addToCount();
+        MealBook.addMealBookIssue(number);
+    }
+    
+    //function used in the checked() function to subtract mealbook compliance issues to the mealBookTracker and update the issue counter.    
+    $scope.removeMealBookIssue = function(number){
+        IssueCounter.subtractFromCount(); 
+        MealBook.subtractMealBookIssue(number);
+    }
+    
+    //function used on the mealBook.html to add or subtract mealbook compliance issues to the mealBookTracker when a checkbox is checked and update the issue counter.
+    $scope.checked= function(number){ 
+        if($scope.mealBook[number].found==true) {
+            $scope.addMealBookIssue(number)
+        } 
+        if($scope.mealBook[number].found==false){
+            $scope.removeMealBookIssue(number)
+        }
+    }
+    
+    //redirect to issues.html
+    $scope.confirm = function(){
+        $state.go('issues');
+    };
+    
+});//end of the issueController Service
