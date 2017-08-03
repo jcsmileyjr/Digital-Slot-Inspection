1d136937aeeaf69a374b8c6ef654df47e625fbc2
JC <jcsmileyjr@gmail.com>
2017-08-02 21:18:09 -0500

    Unit Test Runner, updated app.js and index.html
    The unitTestRunner.spec.html run all the unit tests.
    The app.js and index.html is updated to reflect the new issues and
    mealbook features.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 //Sets the variable myApp as a Angular applicatation named 'DSI'. The module ui-router is injected as a dependency to provide url routing. 
 var myApp = angular.module('DSI', ['ui.router']);
+
+// A .config function is used to provide 'state' and url routing for the app thru the stateProvider (from ui-router).
+myApp.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider, $state) {
+
+    //If no route or link is select, the app redirects to the logIn page
+    $urlRouterProvider.otherwise('/issues');
+
+    $stateProvider
+        //The issues state redirects the user to the issues.html template and issueController via the /issues url. 
+        .state('issues', {
+            url:'/issues',
+            templateUrl: 'issues/issues.html',
+            controller: "issueController"
+        })
+        //The input state redirects the user to the input.html template and inputController via the /input url. 
+        .state('mealBook', {
+            url:'/mealBook',
+            templateUrl: 'mealBook/mealBook.html',
+            controller: "mealBookController"        
+        })
+    
+}]);
