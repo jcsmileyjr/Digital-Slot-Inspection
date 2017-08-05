4e231d34e348c244cf004c37a65ff0b4be44a76d
JC <jcsmileyjr@gmail.com>
2017-08-05 08:49:18 -0500

    Compliance Feature: Service and Unit Test
    A service to provide functionality to the different save and continue
    buttons in the action section. A unit test is provided to test the logic
    in the service.
    
    housekeeping: The action feature is deleted cause it was restructure
    into a combined nav-issue-action feature.

---
+++ js/compliance/complianceService.js
@@ -0,0 +1,67 @@
+/*Update the compliance object so it can be listed and sorted. In the desktop version will save to file. */
+myApp.factory('Compliance', ["SlotMachine", "IssueCounter", "MealBook", function (SlotMachine, IssueCounter, MealBook) {
+
+    //array to hold a saved slot machine with compliance issues 
+    var compliance = [];
+    
+    return {
+        
+        //function to saved slot machine object with compliance issues and marked as completed
+       completed: function(){
+           //current slot machine serial number
+           var currentSlotMachine = SlotMachine.getCurrentSlotMachine();
+           
+           //current slot machine number of compliance issues found
+           var numberOfIssues = IssueCounter.getCurrentCount();
+           
+           //array of MealBook compliance issues
+           var mealBook = MealBook.getMealBookTracker();
+
+           //add to the array the current slot machine identifer and compliance issues
+           compliance.push({"serialNumber":currentSlotMachine.serialNumber, "numberOfIssues":numberOfIssues, "completed": true, "mealBook": mealBook});
+           
+	   },//end of completed function
+        
+        //function to save a slot machine with no compliance issues to the compliance array and marked as completed
+        compliantCompleted: function(){
+        
+            //current slot machine serial number
+            var currentSlotMachine = SlotMachine.getCurrentSlotMachine();
+           
+            //add to the array the current slot machine identifer and compliance issues            
+            compliance.push({"serialNumber":currentSlotMachine.serialNumber, "numberOfIssues":0, "completed": true, "mealBook":[]});
+            
+        },
+        
+        //function to saved slot machine object with compliance issues, but NOT marked as completed        
+        skipCompleted: function(){
+
+           //current slot machine serial number
+           var currentSlotMachine = SlotMachine.getCurrentSlotMachine();
+           
+           //current slot machine number of compliance issues found
+           var numberOfIssues = IssueCounter.getCurrentCount();
+           
+           //array of MealBook compliance issues
+           var mealBook = MealBook.getMealBookTracker();
+
+           //add to the array the current slot machine identifer and compliance issues
+           compliance.push({"serialNumber":currentSlotMachine.serialNumber, "numberOfIssues":numberOfIssues, "completed": false, "mealBook":mealBook});            
+        }, 
+        
+        //return the compliance array of objects
+        getCompliance: function(){
+            return compliance;
+        },
+        
+        //reset all compliance issues
+        resetAllComplianceTrackers: function(){
+            IssueCounter.resetCount();
+            MealBook.resetMealBookTracker();
+        }
+                
+        
+    }/*End of main Return*/
+    
+    
+}]);
