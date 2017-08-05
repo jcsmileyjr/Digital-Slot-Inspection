22496d38be917f7bcf09081881b07cb087473ef2
JC <jcsmileyjr@gmail.com>
2017-08-05 11:40:34 -0500

    Bug: SlotMachine Service not updating controllers
    1. Not sure how I solved this problem. No changes to the files was.
    2. Updated the MealBook to show the current slot machine location and
    serial number.
    3. Deleted some test code mistakenly left in the slotMachineService.js.

--- js/slotMachine/slotMachineService.js
+++ js/slotMachine/slotMachineService.js
@@ -5,19 +5,14 @@ myApp.factory('SlotMachine', function(){
     var slotMachines = [{"location":"1-1-1", "serialNumber":100100, "EPROM":"ASDF1000", "holdPercentage":89.1, "completed":false}, {"location":"1-1-2", "serialNumber":100102, "EPROM":"ASDF1002", "holdPercentage":89.1, "completed":false}, {"location":"1-1-3", "serialNumber":100103, "EPROM":"ASDF1003", "holdPercentage":89.1, "completed":false}, {"location":"1-1-4", "serialNumber":100104, "EPROM":"ASDF1004", "holdPercentage":89.1, "completed":false}, {"location":"1-1-5", "serialNumber":100105, "EPROM":"ASDF1005", "holdPercentage":89.1, "completed":false}, {"location":"1-2-1", "serialNumber":100200, "EPROM":"ASDF2001", "holdPercentage":89.1, "completed":false}, {"location":"1-2-2", "serialNumber":100202, "EPROM":"ASDF2002", "holdPercentage":89.1, "completed":false}, {"location":"1-2-3", "serialNumber":100203, "EPROM":"ASDF2003", "holdPercentage":89.1, "completed":false}, {"location":"1-2-4", "serialNumber":100204, "EPROM":"ASDF2004", "holdPercentage":89.1, "completed":false}, {"location":"1-2-5", "serialNumber":100205, "EPROM":"ASDF2005", "holdPercentage":89.1, "completed":false}];
     
     /*object to hold the current slot machine's location*/
-    var currentLocation = {"location":4};
-    
-    var newSlot = {"slot":slotMachines[currentLocation.location]};
-    
-    
+    var currentLocation = {"location":4};    
     
     return {
         
         /*function to return the current slot machine object*/
         getCurrentSlotMachine: function(){
             /*return the slot machine object at that array position*/
-            //return slotMachines[currentLocation.location];
-            return newSlot.slot;
+            return slotMachines[currentLocation.location];
         },
         
         /*function to return the slot machine before the current slot machine location*/
