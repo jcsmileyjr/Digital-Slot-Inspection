6a8f2dff4d78b51532174af68bd54760a1a37d5e
JC <jcsmileyjr@gmail.com>
2017-08-12 08:24:47 -0500

    Reels, Locks, and Software Feature: Services
    The Reels, Locks, and Software services supports their compliance issue
    with functionality to support the databases, arrays of issues found, and
    html page's checkboxes.

---
+++ js/locks/locksService.js
@@ -0,0 +1,56 @@
+/*a service tracks the Locks compliance issues found during the inspection*/
+myApp.factory('Locks', function () {
+
+    /*array of Locks compliance variables used to tracked which compliance variable was found*/
+    var locks = [{"issue":"CPU Locked", "found":false}, {"issue":"Belly Door Locked", "found":false}, {"issue":"BV Box Locked", "found":false}, {"issue":"Progressive Controller Board Secured", "found":false}, {"issue":"No Funds Visible or Loose", "found":false}];
+    
+    //array used to tracked found Locks compliance issues
+    var locksTracker= [];
+    
+    return {
+        
+        /*function to return the locksTracker array*/
+        getLocksTracker: function(){            
+            return locksTracker;
+        },
+        
+        // function to add an Locks compliance issue to the LocksTracker by corelating the index of the checkbox to the Locks array.
+        addLocksIssue: function(number){
+            locksTracker.push(locks[number].issue);  
+        },
+        
+        // function to subtract an Locks compliance issue from the LocksTracker by corelating the index of the checkbox to the Locks array.
+        subtractLocksIssue: function(number){
+            //check if locksTracker is empty before proceeeding
+            if(locksTracker.length >=0){
+                //search the LocksTracker array
+                for(var i=0;i<locksTracker.length;i++){
+                    //If a locks compliance issue matches a elemnt in the locksTracker array, remove it.
+                    if(locks[number].issue==locksTracker[i]){
+                        locksTracker.splice([i], 1);
+                    }
+                }
+            }
+        },
+        
+        //Function to reset the locksTracker to the original blank array. This is used when the user navigate to a new slot machine.
+        resetLocksTracker: function(){
+            locksTracker = [];
+        },
+        
+        //function to return the list of Lockss compliance issues.
+        getLocks: function(){
+            return locks;
+        },
+        
+        //Loop through each element in the locks array resetting each element issue's found property to false. This is use when a user save and continue to another slot machine.
+        resetLocksCheckbox(){                        
+            for(var i=0;i<locks.length;i++){
+                locks[i].found = false;
+            }
+        }
+        
+    }/*End of main Return*/
+    
+    
+});
---
+++ js/reels/reelsService.js
@@ -0,0 +1,56 @@
+/*a service tracks the Reels compliance issues found during the inspection*/
+myApp.factory('Reels', function () {
+
+    /*array of Reels compliance variables used to tracked which compliance variable was found*/
+    var reels = [{"issue":"Faded Reel Strips", "found":false}, {"issue":"Taped", "found":false}, {"issue":"Wrong Reels", "found":false};
+    
+    //array used to tracked found Reels compliance issues
+    var reelsTracker= [];
+    
+    return {
+        
+        /*function to return the reelsTracker array*/
+        getReelsTracker: function(){            
+            return reelsTracker;
+        },
+        
+        // function to add an Reels compliance issue to the ReelsTracker by corelating the index of the checkbox to the Reels array.
+        addReelsIssue: function(number){
+            reelsTracker.push(reels[number].issue);  
+        },
+        
+        // function to subtract an Reels compliance issue from the ReelsTracker by corelating the index of the checkbox to the Reels array.
+        subtractReelsIssue: function(number){
+            //check if reelsTracker is empty before proceeeding
+            if(reelsTracker.length >=0){
+                //search the ReelsTracker array
+                for(var i=0;i<reelsTracker.length;i++){
+                    //If a reels compliance issue matches a elemnt in the reelsTracker array, remove it.
+                    if(reels[number].issue==reelsTracker[i]){
+                        reelsTracker.splice([i], 1);
+                    }
+                }
+            }
+        },
+        
+        //Function to reset the reelsTracker to the original blank array. This is used when the user navigate to a new slot machine.
+        resetReelsTracker: function(){
+            reelsTracker = [];
+        },
+        
+        //function to return the list of Reelss compliance issues.
+        getReels: function(){
+            return reels;
+        },
+        
+        //Loop through each element in the reels array resetting each element issue's found property to false. This is use when a user save and continue to another slot machine.
+        resetReelsCheckbox(){                        
+            for(var i=0;i<reels.length;i++){
+                reels[i].found = false;
+            }
+        }
+        
+    }/*End of main Return*/
+    
+    
+});
