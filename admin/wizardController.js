34c22c4ee3584063bae476041366b88e49d3d360
JC Smiley <jcsmileyjr@gmail.com>
2017-12-12 03:14:54 -0600

    Update list of casinos on wizard page

--- admin/wizardController.js
+++ admin/wizardController.js
@@ -10,6 +10,11 @@ myApp.controller('wizardController',  function($scope, $state, SlotMachine, Insp
         
         //move the user the nav page to begin the inspection        
         $state.go('nav');
-    }
+       
+    }// end of process function
+    
+        
+        //list of casinos a user can choose from feature on the wizard page
+        $scope.casinos = ["Horseshoe", "Goldstrike", "Roadhouse", "1st Jackpot", "Fitz", "Sam's Town", "Resorts", "Hollywood"];     
     
 }); // end of wizardController
